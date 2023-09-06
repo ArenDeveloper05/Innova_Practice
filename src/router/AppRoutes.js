250c1c4e955afa521104af73e9afc376ac6d727a
anpetrosy <petrosyananeta95@gmail.com>
2023-09-06 13:50:06 +0400

    Reservation Commit

--- src/router/AppRoutes.js
+++ src/router/AppRoutes.js
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import { ROUTER } from "./router";
 import HomePage from "../pages/HomePage";
 import MenuPage from "../pages/MenuPage";
+import ReservationPage from "../pages/ReservationPage";
 const { HOME_PAGE_ROUTE } = ROUTER;
 const { MENU_PAGE_ROUTE } = ROUTER;
+const { RESERVATION_PAGE_ROUTE } = ROUTER;
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={HOME_PAGE_ROUTE} element={<HomePage />} />
       <Route path={MENU_PAGE_ROUTE} element={<MenuPage />} />
+      <Route path={RESERVATION_PAGE_ROUTE} element={<ReservationPage />} />
     </Routes>
   );
 };
