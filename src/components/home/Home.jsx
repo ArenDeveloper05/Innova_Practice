46fe90bfc44acd605fc75ee34697229c62fb5abd
ArenDeveloper05 <antisystem005@mail.ru>
2023-09-07 16:40:15 +0400

    merge

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,27 +1,21 @@
-<<<<<<< HEAD
 import OurSpecialMenu from "./our-special-menu/OurSpecialMenu";
-=======
 import Order from "../order/Order";
 import LatestNews from "./latestNews/LatestNews";
-import BestChefSlider from './bestChefSlider/BestChefSlider'
->>>>>>> 9e8d280ea02b6a2f690801d9135620ac73f6b0e2
+import BestChefSlider from "./bestChefSlider/BestChefSlider";
 import SliderAnimations from "./sliderAnimations/SliderAnimations";
 import Speciality from "./speciality/Speciality";
 
 const Home = () => {
-  return <div>
-    <SliderAnimations />
-<<<<<<< HEAD
-    <Speciality />
-    <OurSpecialMenu />
-
-=======
-    <Order />
-    <Speciality />
-    <BestChefSlider />
-    <LatestNews />
->>>>>>> 9e8d280ea02b6a2f690801d9135620ac73f6b0e2
-  </div>
+  return (
+    <>
+      <SliderAnimations />
+      <Order />
+      <Speciality />
+      <OurSpecialMenu />
+      <BestChefSlider />
+      <LatestNews />
+    </>
+  );
 };
 
-export default Home
+export default Home;
--- src/router/AppRoutes.js
+++ src/router/AppRoutes.js
@@ -2,18 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import { ROUTER } from "./router";
 import HomePage from "../pages/HomePage";
 import BlogLeftSide from "../pages/blog-leftSide/BlogLeftSide";
-const { HOME_PAGE_ROUTE,LEFTSIDE_PAGE_ROUTE } = ROUTER;
 import MenuPage from "../pages/MenuPage";
 import ReservationPage from "../pages/ReservationPage";
-const { HOME_PAGE_ROUTE } = ROUTER;
-const { MENU_PAGE_ROUTE } = ROUTER;
-const { RESERVATION_PAGE_ROUTE } = ROUTER;
+const {
+  HOME_PAGE_ROUTE,
+  LEFTSIDE_PAGE_ROUTE,
+  MENU_PAGE_ROUTE,
+  RESERVATION_PAGE_ROUTE,
+} = ROUTER;
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={HOME_PAGE_ROUTE} element={<HomePage />} />
-      <Route path={LEFTSIDE_PAGE_ROUTE} element={<BlogLeftSide/>}/>
+      <Route path={LEFTSIDE_PAGE_ROUTE} element={<BlogLeftSide />} />
       <Route path={MENU_PAGE_ROUTE} element={<MenuPage />} />
       <Route path={RESERVATION_PAGE_ROUTE} element={<ReservationPage />} />
     </Routes>
