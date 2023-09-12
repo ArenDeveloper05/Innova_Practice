6358a6e81be9dccef2e5cd2d95db06c03b7de960
Ero229 <126801424+Ero229@users.noreply.github.com>
2023-09-12 17:37:32 +0400

    reload Left Side

--- src/router/AppRoutes.js
+++ src/router/AppRoutes.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { ROUTER } from "./router";
 import HomePage from "../pages/HomePage";
-import BlogLeftSide from "../pages/blog-leftSide/BlogLeftSide";
+import LeftSidePage from "../pages/LeftSidePage";
 const { HOME_PAGE_ROUTE,LEFTSIDE_PAGE_ROUTE } = ROUTER;
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={HOME_PAGE_ROUTE} element={<HomePage />} />
-      <Route path={LEFTSIDE_PAGE_ROUTE} element={<BlogLeftSide/>}/>
+      <Route path={LEFTSIDE_PAGE_ROUTE} element={<LeftSidePage/>}/>
     </Routes>
   );
 };
